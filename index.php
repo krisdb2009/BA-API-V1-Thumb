4b7e2c9b1d4e2170381aba7074749e603ff5a104
Dylan K. Bickerstaff <dylan@belowaverage.org>
2018-02-27 22:24:19 -0500

    Modify Thumb

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ Twitter:    @jamiebicknell
 */
 
 define('THUMB_CACHE',           './cache/');    // Path to cache directory (must be writeable)
-define('THUMB_CACHE_AGE',       86400);         // Duration of cached files in seconds
+define('THUMB_CACHE_AGE',       2592000);       // Duration of cached files in seconds
 define('THUMB_BROWSER_CACHE',   true);          // Browser cache true or false
 define('SHARPEN_MIN',           12);            // Minimum sharpen value
 define('SHARPEN_MAX',           28);            // Maximum sharpen value
